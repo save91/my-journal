12ebc075cf78167a1316ba732e4c3d4291e120d3
Saverio Tosi <saverio.tosi@ideato.info>
2018-10-17 23:35:30 +0200

    Add get posts

--- proxy/main.js
+++ proxy/main.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const axios = require('axios')
 const PORT = 8080
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send("Hello world")
+const parseWordPressPost = (wpPost) => {
+    return {
+        title: wpPost.title.rendered
+    }
+}
+
+app.get('/posts', async (req, res) => {
+    const responseFromWp = await axios.get('http://localhost/wp-json/wp/v2/posts')
+    const toReturn = responseFromWp.data.map(post => parseWordPressPost(post))
+    res.send(toReturn)
 })
 
 app.listen(PORT, () => {
