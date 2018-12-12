47e07715f049ae4266890ed9c098838386a290ce
Saverio Tosi <s.tosi@extrategy.net>
2018-12-12 23:51:44 +0100

    Improve post's list

--- my-journal-android/app/src/main/java/com/example/saverio/myjournal/MainActivity.java
+++ my-journal-android/app/src/main/java/com/example/saverio/myjournal/MainActivity.java
@@ -3,20 +3,25 @@ package com.example.saverio.myjournal;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.saverio.myjournal.utilities.NetworkUtils;
 import com.example.saverio.myjournal.utilities.ProxyPostsJsonUtils;
 
 import java.net.URL;
 
-public class MainActivity extends AppCompatActivity {
-    private TextView mPostsTextView;
+public class MainActivity extends AppCompatActivity implements PostAdapter.PostAdapterOnClickHandler {
+
+    private RecyclerView mRecyclerView;
+    private PostAdapter mPostAdapter;
     private TextView mErrorMessageTextView;
     private ProgressBar mLoadingProgressBar;
 
@@ -25,8 +30,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posts);
 
-        mPostsTextView = findViewById(R.id.tv_posts);
+        mRecyclerView = findViewById(R.id.recyclerview_posts);
         mErrorMessageTextView = findViewById(R.id.tv_error_message);
+        /*
+         * LinearLayoutManager can support HORIZONTAL or VERTICAL orientations. The reverse layout
+         * parameter is useful mostly for HORIZONTAL layouts that should reverse for right to left
+         * languages.
+         */
+        LinearLayoutManager layoutManager
+                = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        /*
+         * Use this setting to improve performance if you know that changes in content do not
+         * change the child layout size in the RecyclerView
+         */
+        mRecyclerView.setHasFixedSize(true);
+
+        /*
+         * The PostAdapter is responsible for linking our post data with the Views that
+         * will end up displaying our post data.
+         */
+        mPostAdapter = new PostAdapter(this);
+
+        /* Setting the adapter attaches it to the RecyclerView in our layout. */
+        mRecyclerView.setAdapter(mPostAdapter);
         mLoadingProgressBar = findViewById(R.id.pb_loading);
 
         loadPostsData();
@@ -38,13 +67,18 @@ public class MainActivity extends AppCompatActivity {
         new FetchPostsTask().execute();
     }
 
+    @Override
+    public void onClick(String param) {
+        Toast.makeText(this, param, Toast.LENGTH_LONG).show();
+    }
+
     private void showPostsDataView() {
         mErrorMessageTextView.setVisibility(View.INVISIBLE);
-        mPostsTextView.setVisibility(View.VISIBLE);
+        mRecyclerView.setVisibility(View.VISIBLE);
     }
 
     private void showErrorMessage() {
-        mPostsTextView.setVisibility(View.INVISIBLE);
+        mRecyclerView.setVisibility(View.INVISIBLE);
         mErrorMessageTextView.setVisibility(View.VISIBLE);
     }
 
@@ -80,12 +114,7 @@ public class MainActivity extends AppCompatActivity {
             mLoadingProgressBar.setVisibility(View.INVISIBLE);
             if (postsData != null) {
                 showPostsDataView();
-
-                mPostsTextView.setText("");
-                for(String post : postsData) {
-                    mPostsTextView.append(post + "\n\n\n");
-                }
-
+                mPostAdapter.setPostsData(postsData);
             } else {
                 showErrorMessage();
             }
@@ -106,7 +135,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.action_refresh) {
-            mPostsTextView.setText("");
+            mPostAdapter.setPostsData(null);
             loadPostsData();
             return true;
         }
---
+++ my-journal-android/app/src/main/java/com/example/saverio/myjournal/PostAdapter.java
@@ -0,0 +1,103 @@
+package com.example.saverio.myjournal;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostAdapterViewHolder> {
+
+    private String[] mPostsData;
+    private PostAdapterOnClickHandler mClickHandler;
+
+    interface PostAdapterOnClickHandler {
+        void onClick(String param);
+    }
+
+    public PostAdapter(PostAdapterOnClickHandler clickHandler) {
+        mClickHandler = clickHandler;
+    }
+
+    /**
+     * Cache of the children views for a forecast list item.
+     */
+    public class PostAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public final TextView mPostTextView;
+
+        public PostAdapterViewHolder(View view) {
+            super(view);
+            mPostTextView = view.findViewById(R.id.tv_post_title);
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            mClickHandler.onClick(this.mPostTextView.getText().toString());
+        }
+    }
+
+    /**
+     * This gets called when each new ViewHolder is created. This happens when the RecyclerView
+     * is laid out. Enough ViewHolders will be created to fill the screen and allow for scrolling.
+     *
+     * @param viewGroup The ViewGroup that these ViewHolders are contained within.
+     * @param viewType  If your RecyclerView has more than one type of item (which ours doesn't) you
+     *                  can use this viewType integer to provide a different layout. See
+     *                  {@link android.support.v7.widget.RecyclerView.Adapter#getItemViewType(int)}
+     *                  for more details.
+     * @return A new PostAdapterViewHolder that holds the View for each list item
+     */
+    @Override
+    public PostAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.post_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        return new PostAdapterViewHolder(view);
+    }
+
+    /**
+     * OnBindViewHolder is called by the RecyclerView to display the data at the specified
+     * position. In this method, we update the contents of the ViewHolder to display the weather
+     * details for this particular position, using the "position" argument that is conveniently
+     * passed into us.
+     *
+     * @param PostAdapterViewHolder The ViewHolder which should be updated to represent the
+     *                              contents of the item at the given position in the data set.
+     * @param position              The position of the item within the adapter's data set.
+     */
+    @Override
+    public void onBindViewHolder(PostAdapterViewHolder PostAdapterViewHolder, int position) {
+        String postTitle = mPostsData[position];
+        PostAdapterViewHolder.mPostTextView.setText(postTitle);
+    }
+
+    /**
+     * This method simply returns the number of items to display. It is used behind the scenes
+     * to help layout our Views and for animations.
+     *
+     * @return The number of items available in our forecast
+     */
+    @Override
+    public int getItemCount() {
+        if (null == mPostsData) return 0;
+        return mPostsData.length;
+    }
+
+    /**
+     * This method is used to set the posts on a PostAdapter if we've already
+     * created one. This is handy when we get new data from the web but don't want to create a
+     * new PostAdapter to display it.
+     *
+     * @param postsData The new weather data to be displayed.
+     */
+    public void setPostsData(String[] postsData) {
+        mPostsData = postsData;
+        notifyDataSetChanged();
+    }
+
+}
