c91317522bd5c733a82a13c636798bca3adc74eb
Saverio Tosi <s.tosi@extrategy.net>
2019-01-03 16:26:21 +0100

    Adds informations returned by proxy for post

--- proxy/main.js
+++ proxy/main.js
@@ -3,6 +3,7 @@ const axios = require('axios')
 const he = require('he')
 const environment = require('./environment')
 const PORT = 8080
+const GTM = '000Z'
 
 const app = express()
 const { serverAddress, serverPort, serverProtocol } = environment;
@@ -13,12 +14,24 @@ if (serverPort) {
 }
 
 const parseWordPressPost = (wpPost) => {
+    const data = new Date(`${wpPost.date_gmt}.${GTM}`);
+    const modified = new Date(`${wpPost.modified_gmt}.${GTM}`);
+
     return {
+        id: wpPost.id,
+        author_id: wpPost.author,
+        body: wpPost.content.rendered,
+        categories_id: wpPost.categories,
+        date: data.getTime(),
+        featured_media_id: wpPost.featured_media,
+        link: wpPost.link,
+        modified: modified.getTime(),
+        tags_id: wpPost.tags,
         title: he.decode(wpPost.title.rendered)
     }
 }
 
-app.get('/posts', async (req, res) => {
+app.get('/api/:version/posts', async (req, res) => {
     const postsUrl = `${baseUrl}/wp-json/wp/v2/posts`;
     const responseFromWp = await axios.get(postsUrl)
     const toReturn = responseFromWp.data.map(post => parseWordPressPost(post))
