9f2eeedf72c413c28190c41aad518af8dee0e91e
Saverio Tosi <saverio.tosi@ideato.info>
2019-02-02 18:50:18 +0100

    Adds preference for push notifications

--- my-journal-android/app/src/main/java/com/example/saverio/myjournal/ui/settings/SettingsFragment.java
+++ my-journal-android/app/src/main/java/com/example/saverio/myjournal/ui/settings/SettingsFragment.java
@@ -2,12 +2,14 @@ package com.example.saverio.myjournal.ui.settings;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.EditTextPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceScreen;
 
 import com.example.saverio.myjournal.R;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class SettingsFragment extends PreferenceFragmentCompat implements
         SharedPreferences.OnSharedPreferenceChangeListener {
@@ -49,6 +51,20 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
         if (null != preference) {
             if (preference instanceof EditTextPreference) {
                 setPreferenceSummary(preference, sharedPreferences.getString(key, ""));
+            }
+            if (preference instanceof SwitchPreference) {
+                boolean isOn = sharedPreferences.getBoolean(key, false);
+                if (isOn) {
+                    // The preference key matches the following key for the associated instructor in
+                    // FCM. For example, the key for Lyla is key_lyla (as seen in
+                    // following_squawker.xml). The topic for Lyla's messages is /topics/key_lyla
+
+                    // Subscribe
+                    FirebaseMessaging.getInstance().subscribeToTopic(key);
+                } else {
+                    // Un-subscribe
+                    FirebaseMessaging.getInstance().unsubscribeFromTopic(key);
+                }
             }
         }
     }
